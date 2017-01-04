25669ff187a545ad7f23438483ecc0fdd4902fd8
李昕亮 <lixinliang@yy.com>
2017-01-04 09:53:38 +0800

    src
    update

---
+++ rollup-plugin-bookmark.js
@@ -0,0 +1,10 @@
+module.exports = function bookmark () {
+    return {
+        name : 'bookmark',
+        transformBundle : function ( code ) {
+            return {
+                code : 'javascript:!function(){' + code + '}()';
+            };
+        },
+    };
+};
